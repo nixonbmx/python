5eb4907a4642dea5e4f4ac4bba645cd948aa7653
nixon piñeres <napineres@misena.edu.co>
2021-06-01 11:51:05 -0500

    ejercicios expresiones aritmeticas

---
+++ ejercicios expresiones aritmeticas .py
@@ -0,0 +1,7 @@
+#Sean los siguientes valores para a, b, c, d, e, f
+a = 16; b = 8; c = 7; d = 5; e = 4; f = 2
+#Evaluar
+ejercicio_uno  = a + b * c // d - e ** f 
+ejercicio_dos = (a + b) * c / (d - e) ** f 
+print (ejercicio_uno, " ", ejercicio_dos)
+
